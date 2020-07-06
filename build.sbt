8f9cdf4d73cbee0465e2e656e7721d9e7394af91
deshbandhu <deshbandhu.mishra@gmail.com>
2020-07-06 10:40:05 +0530

    spark 3.0

---
+++ build.sbt
@@ -0,0 +1,14 @@
+name := "Uberv2"
+
+version := "0.1"
+
+scalaVersion := "2.12.8" // don't use 2.12.3, throw Exceptions
+libraryDependencies += "org.apache.spark" %% "spark-core" % "3.0.0"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.0.0"
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "3.0.0"// % "runtime",
+
+// libraryDependencies += "org.apache.spark" %% "spark-mllib-local" % "3.0.0"
+
+resolvers += "Cloudera" at "http://repository.cloudera.com/artifactory/cloudera-repos/"
+
+
---
+++ src/main/scala/UberData.scala
@@ -0,0 +1,50 @@
+
+import org.apache.spark.sql.{SparkSession, _}
+import org.apache.spark.sql.types.{TimestampType, _}
+import org.apache.spark.ml.feature.VectorAssembler
+import org.apache.spark.ml.clustering.KMeans
+
+object UberData extends App {
+
+  case class Uber(dt: String, lat: Double, lon: Double, base: String) extends Serializable
+  var file1: String = "/home/deshbandhu/MCIT_BigData/Uberv2/resource/uber.csv"
+
+  val schema = StructType(Array(
+    StructField("dt", TimestampType, nullable = true),
+    StructField("lat", DoubleType, nullable = true),
+    StructField("lon", DoubleType, nullable = true),
+    StructField("base", StringType, nullable = true)
+  ))
+
+
+  val spark: SparkSession = SparkSession.builder().master("local[*]").appName("uber").getOrCreate()
+  val df: DataFrame = spark.read.option("inferSchema", "false").schema(schema).option("header", "false").csv(file1)
+
+  val featureCols = Array("lat", "lon")
+  val assembler = new VectorAssembler().setInputCols(featureCols).setOutputCol("features")
+  val df2 = assembler.transform(df)
+  val Array(trainingData, testData) = df2.randomSplit(Array(0.7, 0.3), 5043)
+
+
+  val kmeans = new KMeans().setK(10).setFeaturesCol("features").setMaxIter(10)
+  val model = kmeans.fit(trainingData)
+
+  println("Final Centers: =====================================")
+  model.clusterCenters.foreach(println)
+
+  val categories = model.transform(testData)
+  categories.show()
+  categories.createOrReplaceTempView("uber")
+
+  categories.select()
+ println("==================================111111")
+
+  categories.groupBy("prediction").count().show()
+
+  spark.sql("select prediction, count(prediction) as count from uber group by prediction").show
+
+  spark.sql("SELECT hour(uber.dt) as hr,count(prediction) as ct FROM uber group By hour(uber.dt)").show
+ model.write.overwrite().save("/home/deshbandhu/MCIT_BigData/Uberv2/output/savemodel")
+  val res = spark.sql("select dt, lat, lon, base, prediction as cid FROM uber order by dt")
+  res.write.format("json").save("/home/deshbandhu/MCIT_BigData/Uberv2/output/uber.json")
+}
